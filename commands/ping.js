6a0c142f7330c02c1344973136391ede7512b664
Kovacs Alex <kovacs.alex26@gmail.com>
2018-04-06 10:46:49 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,5 @@
 // Example ping command
 exports.run = (client, message, args) => {
-        let uptime = client.uptime;
         message.channel.send(`Pong!`).then(msg => {
             msg.edit(`Pong!\nCurrent server latency: ${msg.createdTimestamp - message.createdTimestamp}ms!`)
         }).catch(err => message.channel.send(err))
