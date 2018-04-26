cdbad044d946130f08c18efd76d6c3d03ac2c7e4
Alex Kovacs <alex@alexthemaster.net>
2018-04-26 22:06:57 +0300

    Usefull eval command

---
+++ commands/eval.js
@@ -0,0 +1,43 @@
+const ms = require('ms');
+
+function Capitalize(word) {
+    let toReturn;
+    let split = word.split('');
+    toReturn = split.shift().toUpperCase();
+    toReturn += split.join('');
+
+    return toReturn;
+}
+
+
+exports.run = async (client, message, args) => {
+    
+    if (!args) return message.channel.send('What should I eval tho? :thinking:');
+
+    let msg = await message.channel.send('Processing...');
+
+    let curDate;
+    let result;
+
+    try {
+    curDate = new Date();
+    result = await eval(args.join(' '));
+    } catch (err) {
+        result = err;
+    }
+
+    if (!result || result.length < 1) return msg.edit('Something went wrong or the eval returned nothing.');
+
+    msg.edit(`**Output:** \`\`\`js\n${result}\`\`\`\n**Type:** \`\`\`${Capitalize(typeof result)}\`\`\`\n:stopwatch: ~${ms(new Date() - curDate, {long: true})}`)
+};
+
+exports.conf = {
+    name: 'eval',
+    description: 'Evaluates Javascript (JS) code.',
+    aliases: ['js-eval'],
+    usage: '<code>',
+
+    enabled: true,
+    guildOnly: false,
+    ownerOnly: true
+}
